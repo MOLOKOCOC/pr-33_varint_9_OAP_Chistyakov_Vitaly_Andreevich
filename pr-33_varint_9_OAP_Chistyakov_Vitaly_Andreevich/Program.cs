37505e75be02113fd8f816afc596b2d98452df64
ДНС <mmm.mch@mail.ru>
2021-10-25 14:24:32 +0500

    Добавьте файлы проекта.

---
+++ pr-33_varint_9_OAP_Chistyakov_Vitaly_Andreevich/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace pr_33_varint_9_OAP_Chistyakov_Vitaly_Andreevich
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Queue<char> a = new Queue<char>();
+            //Queue<char> b = new Queue<char>();
+            //using (StreamReader sr = new StreamReader("numbers.txt")) { 
+            //    while (!sr.EndOfStream){
+            //        char s = (char)sr.Read();
+            //        if (char.IsDigit(s)) b.Enqueue(s);
+            //        else a.Enqueue(s);
+            //    }
+            //}
+            //Console.Write(string.Join("", a));
+            //Console.Write(string.Join("", b));
+            //Console.ReadKey();
+
+
+
+        }
+    }
+}
